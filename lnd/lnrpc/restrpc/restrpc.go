fb22670cdd71eb272e9cae3d089802791d1f18a4
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-02-16 15:47:32 +0200

    added commands

--- lnd/lnrpc/restrpc/restrpc.go
+++ lnd/lnrpc/restrpc/restrpc.go
@@ -32,34 +32,48 @@ type RpcFunc struct {
 }
 
 var rpcFunctions []RpcFunc = []RpcFunc{
+	//WalletUnlocker: Wallet unlock
+	//Will try to unlock the wallet with the password(s) provided
 	{
-		path: "/api/v1/lightning/walletbalance",
-		req:  nil,
-		res:  (*lnrpc.GetAddressBalancesRequest)(nil),
+		path: "/api/v1/wallet/unlock",
+		req:  (*lnrpc.UnlockWalletRequest)(nil),
+		res:  nil,
 		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
-			cc, err := c.withCC()
-			if err != nil {
+			req, ok := m.(*lnrpc.UnlockWalletRequest)
+			if !ok {
+				return nil, er.New("Argument is not a UnlockWalletRequest")
+			}
+			if u, err := c.withUnlocker(); err != nil {
+				return nil, err
+			} else if _, err := u.UnlockWallet0(context.TODO(), req); err != nil {
 				return nil, err
 			}
-			// Get total balance, from txs that have >= 0 confirmations.
-			totalBal, err := cc.Wallet.ConfirmedBalance(0)
-			if err != nil {
-				return nil, err
-			}
-
-			confirmedBal, err := cc.Wallet.ConfirmedBalance(1)
-			if err != nil {
-				return nil, err
-			}
-			// Get unconfirmed balance, from txs with 0 confirmations.
-			unconfirmedBal := totalBal - confirmedBal
-			return &lnrpc.WalletBalanceResponse{
-				TotalBalance:       int64(totalBal),
-				ConfirmedBalance:   int64(confirmedBal),
-				UnconfirmedBalance: int64(unconfirmedBal),
-			}, nil
+			return nil, nil
 		},
 	},
+	//WalletUnlocker: Wallet create
+	//Will try to create/restore wallet
+	{
+		path: "/api/v1/wallet/create",
+		req:  (*lnrpc.CreateWalletRequest)(nil),
+		res:  (*lnrpc.CreateWalletResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			req, ok := m.(*lnrpc.CreateWalletRequest)
+			if !ok {
+				return nil, er.New("Argument is not a CreateWalletRequest")
+			}
+			u, err := c.withUnlocker()
+			if err != nil {
+				return nil, err
+			}
+			resp, errr := u.CreateWallet(context.TODO(), req)
+			if errr != nil {
+				return nil, er.E(errr)
+			}
+			return resp, nil
+		},
+	},
+	//MetaService get info
 	{
 		path: "/api/v1/meta/getinfo",
 		req:  nil,
@@ -191,21 +205,205 @@ var rpcFunctions []RpcFunc = []RpcFunc{
 			}, nil
 		},
 	},
+	//MetaService change wallet password
 	{
-		path: "/api/v1/wallet/unlock",
-		req:  (*lnrpc.UnlockWalletRequest)(nil),
-		res:  nil,
+		path: "/api/v1/meta/changepassword",
+		req:  (*lnrpc.ChangePasswordRequest)(nil),
+		res:  (*lnrpc.ChangePasswordResponse)(nil),
 		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
-			req, ok := m.(*lnrpc.UnlockWalletRequest)
+			req, ok := m.(*lnrpc.ChangePasswordRequest)
 			if !ok {
-				return nil, er.New("Argument is not a UnlockWalletRequest")
+				return nil, er.New("Argument is not a ChangePasswordRequest")
 			}
-			if u, err := c.withUnlocker(); err != nil {
-				return nil, err
-			} else if _, err := u.UnlockWallet0(context.TODO(), req); err != nil {
+			meta, err := c.withMetaServer()
+			if err != nil {
 				return nil, err
 			}
-			return nil, nil
+			resp, errr := meta.ChangePassword(context.TODO(), req)
+			if errr != nil {
+				return nil, er.E(errr)
+			}
+			return resp, nil
+		},
+	},
+	//Wallet balance
+	//requires unlocked wallet -> access to rpcServer
+	{
+		path: "/api/v1/lightning/walletbalance",
+		req:  nil,
+		res:  (*lnrpc.GetAddressBalancesResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.WalletBalance(context.TODO(), nil); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
+		},
+	},
+	//Wallet transactions
+	//requires unlocked wallet -> access to rpcServer
+	{
+		path: "/api/v1/lightning/gettransactions",
+		req:  (*lnrpc.GetTransactionsRequest)(nil),
+		res:  (*lnrpc.TransactionDetails)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			req, ok := m.(*lnrpc.GetTransactionsRequest)
+			if !ok {
+				return nil, er.New("Argument is not a GetTransactionsRequest")
+			}
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.GetTransactions(context.TODO(), req); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
+		},
+	},
+	//New wallet address
+	//requires unlocked wallet -> access to rpcServer
+	{
+		path: "/api/v1/lightning/getnewaddress",
+		req:  (*lnrpc.GetNewAddressRequest)(nil),
+		res:  (*lnrpc.GetNewAddressResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			req, ok := m.(*lnrpc.GetNewAddressRequest)
+			if !ok {
+				return nil, er.New("Argument is not a GetAddressBalancesRequest")
+			}
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.GetNewAddress(context.TODO(), req); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
+		},
+	},
+	//GetAddressBalances
+	{
+		path: "/api/v1/lightning/getaddressbalances",
+		req:  (*lnrpc.GetAddressBalancesRequest)(nil),
+		res:  (*lnrpc.GetAddressBalancesResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			req, ok := m.(*lnrpc.GetAddressBalancesRequest)
+			if !ok {
+				return nil, er.New("Argument is not a GetAddressBalancesRequest")
+			}
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.GetAddressBalances(context.TODO(), req); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
+		},
+	},
+	//Sendfrom
+	{
+		path: "/api/v1/lightning/sendfrom",
+		req:  (*lnrpc.SendFromRequest)(nil),
+		res:  (*lnrpc.SendFromResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			req, ok := m.(*lnrpc.SendFromRequest)
+			if !ok {
+				return nil, er.New("Argument is not a SendFromRequest")
+			}
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.SendFrom(context.TODO(), req); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
+		},
+	},
+	//GetWalletSeed
+	{
+		path: "/api/v1/lightning/getwalletseed",
+		req:  nil,
+		res:  (*lnrpc.GetWalletSeedResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.GetWalletSeed(context.TODO(), nil); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
+		},
+	},
+	//GetTransaction
+	{
+		path: "/api/v1/lightning/gettransaction",
+		req:  (*lnrpc.GetTransactionRequest)(nil),
+		res:  (*lnrpc.GetTransactionResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			req, ok := m.(*lnrpc.GetTransactionRequest)
+			if !ok {
+				return nil, er.New("Argument is not a GetTransactionRequest")
+			}
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.GetTransaction(context.TODO(), req); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
+		},
+	},
+	//Resync
+	{
+		path: "/api/v1/lightning/resync",
+		req:  (*lnrpc.ReSyncChainRequest)(nil),
+		res:  (*lnrpc.ReSyncChainResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			req, ok := m.(*lnrpc.ReSyncChainRequest)
+			if !ok {
+				return nil, er.New("Argument is not a ReSyncChainRequest")
+			}
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.ReSync(context.TODO(), req); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
+		},
+	},
+	//StopResync
+	{
+		path: "/api/v1/lightning/stopresync",
+		req:  nil,
+		res:  (*lnrpc.StopReSyncResponse)(nil),
+		f: func(c *RpcContext, m proto.Message) (proto.Message, er.R) {
+			if server, err := c.withRpcServer(); server != nil {
+				if l, err := server.StopReSync(context.TODO(), nil); err != nil {
+					return nil, er.E(err)
+				} else {
+					return l, nil
+				}
+			} else {
+				return nil, err
+			}
 		},
 	},
 }
@@ -216,6 +414,7 @@ type RpcContext struct {
 	MaybeWallet         *wallet.Wallet
 	MaybeRpcServer      lnrpc.LightningServer
 	MaybeWalletUnlocker *walletunlocker.UnlockerService
+	MaybeMetaService    lnrpc.MetaServiceServer
 }
 
 func with(thing interface{}, name string) er.R {
@@ -238,6 +437,9 @@ func (c *RpcContext) withRpcServer() (lnrpc.LightningServer, er.R) {
 }
 func (c *RpcContext) withUnlocker() (*walletunlocker.UnlockerService, er.R) {
 	return c.MaybeWalletUnlocker, with(c.MaybeWalletUnlocker, "UnlockerService")
+}
+func (c *RpcContext) withMetaServer() (lnrpc.MetaServiceServer, er.R) {
+	return c.MaybeMetaService, with(c.MaybeMetaService, "MetaServiceService")
 }
 
 type SimpleHandler struct {
